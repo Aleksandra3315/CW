641208d08f7a5834ee1e17faa93427ed22ca57eb
Aleksandra3315 <122559621+Aleksandra3315@users.noreply.github.com>
2023-03-13 21:42:14 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿//Console.Clear();
+
+string[] text = { "Whether", "the", "weather", "is", "fine", ",", "Whether the weather", "is", "not" };
+
+string[] ToStringWithMax3Chars(string[] str)
+{
+    int count = 0;
+    string[] temp = new string[str.Length];
+    for (int i = 0; i < str.Length; i++)
+    {
+        char[] a = text[i].ToCharArray();
+        if (a.Length <= 3)
+        {
+            temp[count] = text[i];
+            count++;
+        }
+    }
+    string[] result = new string[count];
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = temp[i];
+    }
+    System.Console.WriteLine($"The number of items in the final array: {count}");
+    return result;
+}
+
+string [] result = ToStringWithMax3Chars (text);
+
+System.Console.Write("The resulting string is: ");
+System.Console.WriteLine(String.Join("_", result));
+
